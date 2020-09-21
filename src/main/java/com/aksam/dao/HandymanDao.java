46c35b602043b964750b501ccb656305521d4e1f
Akanimo <akanimo.okure@uis.cam.ac.uk>
2020-09-21 22:19:52 +0100

    handyman dao and dao implementation in progress

--- src/main/java/com/aksam/dao/HandymanDao.java
+++ src/main/java/com/aksam/dao/HandymanDao.java
@@ -1,5 +1,17 @@
 package com.aksam.dao;
 
+import java.util.List;
+
+import com.aksam.entity.Handyman;
+
 public interface HandymanDao {
+	
+	public void saveHandyman( Handyman handyman);
+	public List<Handyman> getAllHandyman();
+	public Handyman getHandymanById( int id);
+	public void updateHandyman(  int id);
+	public void getHanymanByLocation(Handyman handyman, String state, String lga);
+	public void deleteHandyman(  int id);
+	
 
 }
--- src/main/java/com/aksam/dao/HandymanDaoImpl.java
+++ src/main/java/com/aksam/dao/HandymanDaoImpl.java
@@ -1,5 +1,62 @@
 package com.aksam.dao;
 
-public class HandymanDaoImpl {
+import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.aksam.entity.Handyman;
+import com.aksam.entity.User;
+
+@Repository
+public class HandymanDaoImpl {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public void saveHandyman( Handyman handyman) {
+		
+		// create currentSession
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// use session saveorUpdate method to save
+		 currentSession.saveOrUpdate(handyman);
+		      
+}
+	
+	public List<Handyman> getAllHandyman(){
+		// create currentSession
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create query and execute
+	Query q =  currentSession.createQuery("from Handyman h, Handyman.class");
+	
+	        
+	List<Handyman>result = q.getResultList();
+		
+
+		
+		return result;
+	}
+	
+	
+	
+
+	public Handyman FindById( int id){
+		
+		// get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// Now retrieve/read from database using the primary key
+		
+		Handyman theHandyman = currentSession.get(Handyman.class, id);
+		
+		return theHandyman;
+		   
+	}
+	
 }
